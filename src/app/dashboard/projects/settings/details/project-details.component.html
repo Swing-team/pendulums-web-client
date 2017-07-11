c9b6904733b1522c6fefc9417e2f92a796e1dddc
Armin Ghoreishi <armin.ghoreishi@gmail.com>
2017-07-11 18:18:47 +0430

    Added project settings modal

---
+++ src/app/dashboard/projects/settings/details/project-details.component.html
@@ -0,0 +1,1 @@
+<h1>Project Details</h1>
---
+++ src/app/dashboard/projects/settings/details/project-details.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'project-details',
+  templateUrl: './project-details.component.html',
+  styleUrls: ['./project-details.component.sass']
+})
+
+export class ProjectDetailsComponent {
+  constructor() {}
+
+}
