79d5d92260238dfe931bea9c4c84d4827c457f23
Armin Ghoreishi <armin.ghoreishi@gmail.com>
2017-07-11 18:18:47 +0430

    Added project settings modal

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,11 @@ import { AppRoutingModule }       from '../app-routing.module';
 import { SharedModule }           from '../shared/shared.module';
 
 import { CreateProjectComponent } from './projects/create-project/create-project.component';
+import { ProjectSettingsModalComponent } from './projects/settings/modal/project-settings-modal.component';
+import { ProjectDetailsComponent } from './projects/settings/details/project-details.component';
+import { ProjectMembersComponent } from './projects/settings/members/project-members.component';
+import { ProjectPendingInvitationsComponent } from './projects/settings/pending-invitations/project-pending-invitations.component';
+import { DangerousActionsComponent } from './projects/settings/dangerous-actions/project-dangerous-actions.component';
 import { DashboardComponent }     from './dashboard.component';
 
 import { ProjectService }         from './shared/projects.service';
@@ -14,6 +19,11 @@ import { ProjectService }         from './shared/projects.service';
   ],
   declarations: [
     CreateProjectComponent,
+    ProjectSettingsModalComponent,
+    ProjectDetailsComponent,
+    ProjectMembersComponent,
+    ProjectPendingInvitationsComponent,
+    DangerousActionsComponent,
     DashboardComponent
   ],
   providers: [
---
+++ src/app/dashboard/projects/settings/pending-invitations/project-pending-invitations.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'project-pending-invitations',
+  templateUrl: './project-pending-invitations.component.html',
+  styleUrls: ['./project-pending-invitations.component.sass']
+})
+
+export class ProjectPendingInvitationsComponent {
+  constructor() {
+  }
+}
