06bc47bec9a5eb9073a575a3da6dc16be7baed47
Armin Ghoreishi <armin.ghoreishi@gmail.com>
2017-07-15 12:37:01 +0430

    Added pending invitations UI in settings

--- src/app/dashboard/projects/settings/pending-invitations/project-pending-invitations.component.ts
+++ src/app/dashboard/projects/settings/pending-invitations/project-pending-invitations.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import * as _ from 'lodash';
+import {Project} from '../../../../shared/state/project/project.model';
 
 @Component({
   selector: 'project-pending-invitations',
@@ -7,6 +9,19 @@ import {Component} from '@angular/core';
 })
 
 export class ProjectPendingInvitationsComponent {
+  roles = ['team member', 'admin'];
+  private user = {email: null, role: this.roles[0], hash: null};
+  private project: Project = new Project();
+
   constructor() {
+  }
+
+  invite() {
+    this.project.invitedUsers.push(_.cloneDeep(this.user));
+    this.user = {email: null, role: this.roles[0], hash: null};
+  }
+
+  delete(invitedUserId) {
+    this.project.invitedUsers.splice(invitedUserId, 1);
   }
 }
