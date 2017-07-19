760ea773dcaaaa093f5a7826864f546a21c6563b
Armin Ghoreishi <armin.ghoreishi@gmail.com>
2017-07-19 14:24:43 +0430

    Added update apiCall for project detials

--- src/app/dashboard/projects/create-project/create-project.component.ts
+++ src/app/dashboard/projects/create-project/create-project.component.ts
@@ -48,10 +48,12 @@ export class CreateProjectComponent {
       delete this.project['image'];
       const formData = new FormData();
       formData.append('project', JSON.stringify(this.project));
+      // FIXME: arminghm 19 Jul 2017 mozGetAsFile() method is deprecated
       formData.append('image', this.projectImageCanvasElem.nativeElement.mozGetAsFile('projectImage.png'));
 
-      this.projectServices.create(formData).then((project) => {
-        this.store.dispatch(this.projectsActions.addProject(project));
+      console.log('image: ' , this.projectImageCanvasElem.nativeElement.mozGetAsFile('projectImage.png'));
+
+      this.projectServices.create(formData).then(() => {
         console.log('project added successfully');
         this.project = new Project();
         this.modalIsActive = false;
@@ -109,6 +111,7 @@ export class CreateProjectComponent {
     return true;
   }
   resizeImage() {
+    // FIXME: arminghm 19 Jul 2017 Resized jpeg images has larger size than nonResized jpeg
     const MAX_WIDTH = 500;
     const MAX_HEIGHT = 500;
     let context = this.projectImageCanvasElem.nativeElement.getContext('2d');
--- src/app/dashboard/shared/projects.service.ts
+++ src/app/dashboard/shared/projects.service.ts
@@ -1,25 +1,36 @@
-import { Inject, Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import {Inject, Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
-import { APP_CONFIG } from '../../app.config';
-import {Project} from '../../shared/state/project/project.model';
+import {APP_CONFIG} from '../../app.config';
 
 @Injectable()
 export class ProjectService {
   private headers = new Headers({'Content-Type': 'application/json'});
 
-  constructor(
-    private http: Http, @Inject(APP_CONFIG) private config) { }
+  constructor(private http: Http, @Inject(APP_CONFIG) private config) {
+  }
 
   create(project): Promise<Project> {
     return this.http
       .post(this.config.apiEndpoint + '/projects', project, {withCredentials: true})
       .toPromise()
-      .then(response => response.json() as Project)
+      .then((response) => {
+        console.log(response);
+      })
       .catch(this.handleError);
   }
+
+  update(project, projectId): Promise<any> {
+    return this.http
+      .put(this.config.apiEndpoint + '/projects/' + projectId, project, {withCredentials: true})
+      .toPromise()
+      .then(response => {
+      })
+      .catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error);
