9f83e26a7ce79c0ce2a3df42cf3b43ee96b01e83
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2017-07-19 14:25:21 +0430

    Added SideMenu Component

---
+++ src/app/core/side-menu/side-menu.component.ts
@@ -0,0 +1,11 @@
+import {Component, Inject} from '@angular/core';
+
+@Component({
+  selector: 'side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.sass']
+})
+
+export class SideMenuComponent {
+
+}
