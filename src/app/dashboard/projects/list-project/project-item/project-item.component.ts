91f0af1546f26a05aec290b505a026cac6d02af2
mahsa_ml90 <molaee.mahsa@gmail.com>
2017-07-20 17:22:26 +0430

    project list css has been completed

--- src/app/dashboard/projects/list-project/project-item/project-item.component.ts
+++ src/app/dashboard/projects/list-project/project-item/project-item.component.ts
@@ -1,22 +1,23 @@
 import {Component, Inject, Input}        from '@angular/core';
 import {APP_CONFIG} from '../../../../app.config';
+import {Project} from '../../../../shared/state/project/project.model';
 
 @Component({
   selector: 'project-item',
   templateUrl: './project-item.component.html',
   styleUrls: ['./project-item.component.sass'],
 })
-
 export class ProjectItemComponent {
-  @Input() project: any;
-  @Input() index: any;
+  @Input() project: Project;
+  private startTask: boolean;
+  private taskName: string;
   private activities = [
     {
       name: 'first activity',
       hour: '1',
     },
     {
-      name: 'second activity',
+      name: 'second activity second activity',
       hour: '2',
     },
     {
@@ -25,5 +26,11 @@ export class ProjectItemComponent {
     },
   ];
   constructor (@Inject(APP_CONFIG) private config) {
+    this.startTask = false;
+    this.taskName = 'Untitled task';
+  }
+
+  toggleTimer() {
+    this.startTask = !this.startTask;
   }
 }
