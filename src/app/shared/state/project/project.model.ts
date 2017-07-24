cedd91d4334bea931638892e97fe9f14a011d559
Armin Ghoreishi <armin.ghoreishi@gmail.com>
2017-07-24 12:37:40 +0430

    Removed the settings if the user is not owner or admin.
    Added TeamMemberModel.

--- src/app/shared/state/project/project.model.ts
+++ src/app/shared/state/project/project.model.ts
@@ -1,10 +1,13 @@
+import {TeamMember} from '../team-member/team-member.model';
+
 export class Project {
   id: string = null;
   name: string = null;
   image: string = null;
   invitedUsers = [];
-  owner: object;
-  teamMembers: Array<object>
+  owner: TeamMember;
+  teamMembers: Array<TeamMember>;
+  admins: Array<TeamMember>;
 
   constructor() {}
 }
