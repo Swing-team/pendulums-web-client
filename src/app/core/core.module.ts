1c3eb03c27cb96cbb4d4137aa87bb9997bd611f1
mahsa_ml90 <molaee.mahsa@gmail.com>
2017-07-24 18:37:57 +0430

    added user profile (WIP)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -17,6 +17,7 @@ import { ToolbarComponent }             from './toolbar/toolbar.component';
 import { SharedModule }                 from '../shared/shared.module';
 import { SideMenuComponent }            from './side-menu/side-menu.component';
 import { ModalComponent }               from './modal/modal.component';
+import {UserProfileComponent}           from './side-menu/user-profile/user-profile.component';
 
 @NgModule({
   imports:      [
@@ -27,7 +28,8 @@ import { ModalComponent }               from './modal/modal.component';
   declarations: [
     ToolbarComponent,
     SideMenuComponent,
-    ModalComponent
+    ModalComponent,
+    UserProfileComponent
   ],
   exports:      [
     ToolbarComponent,
@@ -40,7 +42,7 @@ import { ModalComponent }               from './modal/modal.component';
     UserService,
     AuthenticationService,
     UserActions,
-    ProjectsActions
+    ProjectsActions,
   ],
   entryComponents: [
     ModalComponent
