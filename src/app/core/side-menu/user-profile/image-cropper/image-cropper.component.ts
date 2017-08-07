d89cc0b57e4b1278ad0cfda6a8e12c11ddb4854d
mahsa_ml90 <molaee.mahsa@gmail.com>
2017-08-07 11:39:22 +0430

    state updated after user profile updated

---
+++ src/app/core/side-menu/user-profile/image-cropper/image-cropper.component.ts
@@ -0,0 +1,80 @@
+import {Component, ViewChild} from '@angular/core';
+import {ImageCropperComponent, CropperSettings} from 'ng2-img-cropper';
+import {ModalService} from '../../../modal/modal.service';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../../shared/state/appState';
+import {UserActions} from '../../../../shared/state/user/user.actions';
+import {UserService} from '../../../user.service';
+
+@Component({
+  selector: 'image-cropper',
+  templateUrl: './image-cropper.component.html',
+  styleUrls: ['./image-cropper.component.sass'],
+})
+export class ImgCropperComponent {
+  profileData: any;
+  croppedImageFile: any;
+  cropperSettings: CropperSettings;
+  @ViewChild('cropper', undefined)
+  cropper: ImageCropperComponent;
+
+  constructor(private modalService: ModalService,
+              private store: Store<AppState>,
+              private userActions: UserActions,
+              private UserService: UserService,) {
+    this.cropperSettings = new CropperSettings();
+    this.cropperSettings.noFileInput = true;
+    this.cropperSettings.rounded = true;
+    this.cropperSettings.keepAspect = true;
+    this.profileData = {};
+  }
+
+  fileChangeListener($event) {
+    const image: any = new Image();
+    const file: File = $event.target.files[0];
+    const myReader: FileReader = new FileReader();
+    const that = this;
+    myReader.onloadend = function (loadEvent: any) {
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
+    };
+    myReader.readAsDataURL(file);
+  }
+
+  cancel() {
+    this.modalService.close();
+  }
+
+  save() {
+    this.croppedImageFile = this.base64ToFile(this.profileData.image);
+
+    const formData = new FormData();
+    formData.append('user', JSON.stringify({}));
+    formData.append('image', this.croppedImageFile);
+    this.UserService.update(formData).then((user) => {
+      this.store.dispatch(this.userActions.loadUser(user));
+    })
+      .catch(error => {
+        console.log('error is: ', error);
+      });
+    this.modalService.close();
+  }
+
+  base64ToFile(inputBase64) {
+    let base64String = inputBase64.split(',')[1];
+    let binaryString = atob(base64String);
+    let len = binaryString.length;
+    let bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return new File([bytes.buffer], 'image.jpg');
+  }
+
+  getBase64(file, callBack) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callBack(reader.result);
+    reader.onerror = (error) => console.log('Error: ', error);
+  }
+}
