29536eb94f1a421b71e756f161ae20fa019c020d
mahsa_ml90 <molaee.mahsa@gmail.com>
2017-08-07 11:39:22 +0430

    state updated after user profile updated

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -18,6 +18,8 @@ import { SharedModule }                 from '../shared/shared.module';
 import { SideMenuComponent }            from './side-menu/side-menu.component';
 import { ModalComponent }               from './modal/modal.component';
 import {UserProfileComponent}           from './side-menu/user-profile/user-profile.component';
+import {ImgCropperComponent} from './side-menu/user-profile/image-cropper/image-cropper.component';
+import {ImageCropperComponent} from 'ng2-img-cropper';
 
 @NgModule({
   imports:      [
@@ -29,7 +31,9 @@ import {UserProfileComponent}           from './side-menu/user-profile/user-prof
     ToolbarComponent,
     SideMenuComponent,
     ModalComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    ImgCropperComponent,
+    ImageCropperComponent
   ],
   exports:      [
     ToolbarComponent,
@@ -45,7 +49,8 @@ import {UserProfileComponent}           from './side-menu/user-profile/user-prof
     ProjectsActions,
   ],
   entryComponents: [
-    ModalComponent
+    ModalComponent,
+    ImgCropperComponent
   ]
 })
 export class CoreModule {
