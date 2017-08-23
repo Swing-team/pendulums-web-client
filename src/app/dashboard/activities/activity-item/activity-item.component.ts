e1b095daf0f149ee0f252b1c20af1604a093af05
mahsa <molaee.mahsa@gmail.com>
2017-08-23 18:41:12 +0430

    Implemented group by activity list

--- src/app/dashboard/activities/activity-item/activity-item.component.ts
+++ src/app/dashboard/activities/activity-item/activity-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, Inject, Input } from '@angular/core';
+import {
+  Component, EventEmitter,
+  Inject, Input, OnInit, Output
+}                                   from '@angular/core';
 import { APP_CONFIG }               from '../../../app.config';
 import { ActivityService }          from '../../../shared/activity/activity.service';
 import { Activity }                 from '../../../shared/state/activity/activity.model';
@@ -8,20 +11,57 @@ import { Activity }                 from '../../../shared/state/activity/activit
   templateUrl: './activity-item.component.html',
   styleUrls: ['./activity-item.component.sass']
 })
-export class ActivityItemComponent {
+export class ActivityItemComponent implements OnInit {
   @Input() activity: Activity;
+  @Output() onDeleteClicked = new EventEmitter();
+  private from: string;
+  private to: string;
+  private duration: string;
 
   constructor (@Inject(APP_CONFIG) private config,
-               private activityService: ActivityService,) {}
+               private activityService: ActivityService) {}
+
+  ngOnInit() {
+    const fromDate = new Date(Number(this.activity.startedAt));
+    this.from = fromDate.getHours() + ':' + fromDate.getMinutes();
+    const toDate = new Date(Number(this.activity.stoppedAt));
+    this.to = toDate.getHours() + ':' + toDate.getMinutes();
+    this.duration = this.calculateActivityDuration(this.activity);
+  }
 
   delete() {
-    this.activityService.delete(this.activity.project, this.activity.id).then((activity) => {
-
-    })
-      .catch(error => {
-        console.log('error is: ', error);
-      });
+    this.onDeleteClicked.emit();
   }
+
+  calculateActivityDuration (activity) {
+    const duration = Number(activity.stoppedAt) - Number(activity.startedAt);
+    let result: string;
+    let x = duration / 1000;
+    const seconds = Math.floor(x % 60);
+    // minutes
+    x /= 60;
+    const minutes = Math.floor(x % 60);
+    // hours
+    x /= 60;
+    const hours = Math.floor(x);
+
+    if (hours !== 0) {
+      if (minutes < 10) {
+        result = hours + ':0' + minutes ;
+      } else {
+        result = hours + ':' + minutes ;
+      }
+    }
+
+    if (minutes !== 0 && hours === 0) {
+      result = minutes + ' min' ;
+    }
+
+    if (minutes === 0 && hours === 0) {
+      result = seconds + ' sec';
+    }
+    return result;
+  };
 }
 
 
