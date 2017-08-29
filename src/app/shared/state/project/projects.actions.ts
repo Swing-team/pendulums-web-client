f4c7b44787fd81a97b595dcb170cf835e486a381
Armin Ghoreishi <armin.ghoreishi@gmail.com>
2017-08-29 14:35:27 +0430

    Added confirmation on member and invited users deletion

--- src/app/shared/state/project/projects.actions.ts
+++ src/app/shared/state/project/projects.actions.ts
@@ -10,6 +10,11 @@ export class ProjectsActions {
   static CLEAR_PROJECTS = 'CLEAR_PROJECTS';
   static ADD_PROJECT = 'ADD_PROJECT';
   static REMOVE_PROJECT = 'REMOVE_PROJECT';
+  static ADD_INVITED_USER = 'ADD_INVITED_USER';
+  static UPDATE_PROJECT = 'UPDATE_PROJECT';
+  static REMOVE_INVITED_USER = 'REMOVE_INVITED_USER';
+  static REMOVE_MEMBER = 'REMOVE_MEMBER';
+  static CHANGE_MEMBER_ROLE = 'CHANGE_MEMBER_ROLE';
   static UPDATE_PROJECT_ACTIVITIES = 'UPDATE_PROJECT_ACTIVITIES';
 
   loadProjects(projects: Projects): Action {
@@ -36,6 +41,54 @@ export class ProjectsActions {
     return {
       type: ProjectsActions.REMOVE_PROJECT,
       payload: project
+    };
+  }
+
+  updateProject(project: Project): Action {
+    return {
+      type: ProjectsActions.UPDATE_PROJECT,
+      payload: project
+    };
+  }
+
+  addInvitedUser(projectId: String, invitedUser: {email, role}): Action {
+    return {
+      type: ProjectsActions.ADD_INVITED_USER,
+      payload: {
+        projectId,
+        invitedUser
+      }
+    };
+  }
+
+  removeInvitedUser(projectId: String, invitedUser: {email, role}): Action {
+    return {
+      type: ProjectsActions.REMOVE_INVITED_USER,
+      payload: {
+        projectId,
+        invitedUser
+      }
+    };
+  }
+
+  removeMember(projectId: String, memberId: String): Action {
+    return {
+      type: ProjectsActions.REMOVE_MEMBER,
+      payload: {
+        projectId,
+        memberId
+      }
+    };
+  }
+
+  changeMemberRole(projectId: String, userId: String, updatedRole: String) {
+    return {
+      type: ProjectsActions.CHANGE_MEMBER_ROLE,
+      payload: {
+        projectId,
+        userId,
+        updatedRole
+      }
     };
   }
 
--- src/app/shared/state/project/projects.reducer.ts
+++ src/app/shared/state/project/projects.reducer.ts
@@ -39,6 +39,72 @@ export default function reducer(state = initialState, action: Action) {
       return newState;
     }
 
+    case ProjectsActions.ADD_INVITED_USER: {
+      const newState = JSON.parse(JSON.stringify(state));
+      const updateProject = newState.entities[action.payload.projectId];
+      updateProject.invitedUsers.push(action.payload.invitedUser);
+      return newState;
+    }
+    case ProjectsActions.UPDATE_PROJECT: {
+      const newState = JSON.parse(JSON.stringify(state));
+      newState.entities[action.payload.id] = action.payload;
+      return newState;
+    }
+    case ProjectsActions.REMOVE_INVITED_USER: {
+      const newState = JSON.parse(JSON.stringify(state));
+      let invitedUserIndexToRemove;
+      newState.entities[action.payload.projectId].invitedUsers.map((invitedUser, index) => {
+        if (invitedUser.email === action.payload.invitedUser.email) {
+          invitedUserIndexToRemove = index;
+        }
+      });
+      newState.entities[action.payload.projectId].invitedUsers.splice(invitedUserIndexToRemove, 1);
+      return newState;
+    }
+
+    case ProjectsActions.REMOVE_MEMBER: {
+      const newState = JSON.parse(JSON.stringify(state));
+      let removedMemberIndex;
+      newState.entities[action.payload.projectId].teamMembers.map((member, index) => {
+        if (member.id === action.payload.memberId) {
+          removedMemberIndex = index;
+        }
+      });
+      newState.entities[action.payload.projectId].teamMembers.splice(removedMemberIndex, 1);
+      return newState;
+    }
+
+    case ProjectsActions.CHANGE_MEMBER_ROLE: {
+      const newState = JSON.parse(JSON.stringify(state));
+      const updatedProject = newState.entities[action.payload.projectId];
+      if (action.payload.updatedRole === 'admin') {
+        // This means that a team-member becomes an admin.
+        updatedProject.teamMembers.map(member => {
+          if (member.id === action.payload.userId) {
+            if (updatedProject.admins) {
+              updatedProject.admins.push(member);
+            } else {
+              updatedProject.admins = [];
+              updatedProject.admins.push(member);
+            }
+          }
+        });
+      } else {
+        // This means that an admin becomes a team-member.
+        let updatedMemberIndex = null;
+        updatedProject.admins.map((admin, index) => {
+          if (admin.id === action.payload.userId) {
+            updatedMemberIndex = index;
+          }
+        });
+        if (updatedMemberIndex !== null) {
+          updatedProject.admins.splice(updatedMemberIndex, 1);
+        }
+      }
+
+      return newState;
+    }
+
     case ProjectsActions.UPDATE_PROJECT_ACTIVITIES: {
       const newState = JSON.parse(JSON.stringify(state));
       newState.entities[action.payload.projectId].activities =
