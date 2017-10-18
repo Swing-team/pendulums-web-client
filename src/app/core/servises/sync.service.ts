4216aa6f3aeba4b0dfb1a621e735a58d28b94494
mahsa <molaee.mahsa@gmail.com>
2017-10-18 16:38:29 +0330

    Websocket for sync service

--- src/app/core/servises/sync.service.ts
+++ src/app/core/servises/sync.service.ts
@@ -1,12 +1,17 @@
 import 'rxjs/add/operator/toPromise';
-import { Inject, Injectable }           from '@angular/core';
+import {Inject, Injectable, OnInit} from '@angular/core';
 import { Http }                         from '@angular/http';
 import { APP_CONFIG }                   from '../../app.config';
+import { io }  from 'socket.io-client';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../shared/state/appState';
 
 @Injectable()
-export class SyncService {
+export class SyncService implements OnInit {
+  private socket = null;
   constructor(private http: Http,
-              @Inject(APP_CONFIG) private config) {
+              @Inject(APP_CONFIG) private config,
+              private store: Store<AppState>,) {
   }
 
   syncData(data): Promise<any> {
@@ -15,6 +20,19 @@ export class SyncService {
       .toPromise()
       .then(() => console.log('Offline activities has been sync with server successfully :)'))
       .catch(this.handleError);
+  }
+
+  ngOnInit(): void {
+    this.socket = io.connect(this.config.socketEndpoint,
+      {transports: ['websocket'], upgrade: false});
+
+    this.socket.on('connected', function (data) {
+      // make connection status true
+    });
+
+    this.socket.on('connect_error', function (error) {
+      // make connection status false
+    });
   }
 
   private handleError(error: any): Promise<any> {
