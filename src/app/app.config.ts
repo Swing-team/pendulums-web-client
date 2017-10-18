35ac4130b9338930e4daf2bb8115bda0dd60b504
mahsa <molaee.mahsa@gmail.com>
2017-10-18 16:38:29 +0330

    Websocket for sync service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { OpaqueToken } from '@angular/core';
 export let APP_CONFIG = new OpaqueToken('app.config');
 
 export const CONFIG = {
+  socketEndpoint: '',
   apiEndpoint: 'http://localhost:1337',
   httpOptions: { withCredentials: true },
   // imagesEndpoint: 'http://websiteIp/images',
