a7e1bd9c1d68fb6207393d8a641b36436a6820ef
mahsa <molaee.mahsa@gmail.com>
2018-01-21 00:09:23 +0330

    Bug Fixed: in side menu the selected page is not highlighted (TG-7 #ready-for-test)

--- src/app/core/services/router-guards/anonymous-guard.service.ts
+++ src/app/core/services/router-guards/anonymous-guard.service.ts
@@ -17,6 +17,8 @@ export class AnonymousGuardService implements CanActivate {
     this.user.subscribe((user: User) => {
       if (user.id) {
         this.userId = user.id;
+      } else {
+        this.userId = null;
       }
     });
   }
