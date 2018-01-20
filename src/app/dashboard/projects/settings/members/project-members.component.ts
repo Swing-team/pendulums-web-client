65ffaf5ab946f5e92806e3b042eb4cb8e34b146c
mahsa <molaee.mahsa@gmail.com>
2018-01-20 21:47:45 +0330

    Bug Fixed: team members should see the owner as owner and admin should not be able to edit owner's role! (TG-179 #ready-for-test)

--- src/app/dashboard/projects/settings/members/project-members.component.ts
+++ src/app/dashboard/projects/settings/members/project-members.component.ts
@@ -1,15 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Project} from '../../../../shared/state/project/project.model';
-import {ProjectService} from '../../../shared/projects.service';
-import {userRoleInProject} from '../../../shared/utils';
-import {cloneDeep} from 'lodash';
-import {User} from '../../../../shared/state/user/user.model';
-import {Md5} from 'ts-md5/dist/md5';
-import {TeamMember} from '../../../../shared/state/team-member/team-member.model';
-import {log} from 'util';
-import {AppState} from '../../../../shared/state/appState';
-import {Store} from '@ngrx/store';
-import {ProjectsActions} from '../../../../shared/state/project/projects.actions';
+import { Component, Input, OnInit }   from '@angular/core';
+import { Project }                    from '../../../../shared/state/project/project.model';
+import { ProjectService }             from '../../../shared/projects.service';
+import { userRoleInProject }          from '../../../shared/utils';
+import { cloneDeep }                  from 'lodash';
+import { User }                       from '../../../../shared/state/user/user.model';
+import { Md5 }                        from 'ts-md5/dist/md5';
+import { TeamMember }                 from '../../../../shared/state/team-member/team-member.model';
+import { AppState }                   from '../../../../shared/state/appState';
+import { Store }                      from '@ngrx/store';
+import { ProjectsActions }            from '../../../../shared/state/project/projects.actions';
 
 @Component({
   selector: 'project-members',
@@ -30,38 +29,48 @@ export class ProjectMembersComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.project);
+    console.log('this.project:', this.project);
     let userIndexInMembersList = null;
     const projectMembers = cloneDeep(this.project.teamMembers);
-    console.log('deep copy', projectMembers);
-    const adminIds = [];
+    const adminsAndOwnerIds = [];
     projectMembers.map((teamMember, index) => {
       if (teamMember.id === this.user.id) {
         userIndexInMembersList = index;
       }
     });
+
     if (userIndexInMembersList !== null) {
       projectMembers.splice(userIndexInMembersList, 1);
     }
+
+    // here we specify admins from team members
     if (this.project.admins) {
       this.project.admins.map(admin => {
         if (admin.id !== this.user.id) {
           this.members.push({...admin, role: 'admin'});
-          adminIds.push(admin.id);
+          adminsAndOwnerIds.push(admin.id);
         }
       });
     }
 
+    // here we specify owner from team members
+    if (this.project.owner) {
+      if (this.project.owner.id !== this.user.id) {
+        this.members.push({...this.project.owner, role: 'owner'});
+        adminsAndOwnerIds.push(this.project.owner.id);
+      }
+    }
+
     projectMembers.map(member => {
       if (this.project.admins) {
-        if (adminIds.indexOf(member.id) === -1) {
+        if (adminsAndOwnerIds.indexOf(member.id) === -1) {
           this.members.push({...member, role: 'team member'});
         }
       } else {
         this.members.push({...member, role: 'team member'});
       }
     });
-    console.log(this.members);
+    console.log('this.members:', this.members);
   }
 
   userEmailHash(email) {
