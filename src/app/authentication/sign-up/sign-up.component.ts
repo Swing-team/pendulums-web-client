cc4331efb7da899ce69d9ab84be7c707a3b84051
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2018-01-26 19:58:10 +0330

    Applied more text changes

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -27,7 +27,7 @@ export class SignUpComponent {
           this.submitted = false;
           console.log('error is: ', error);
           if (error.status === 400) {
-            this.errorMessage = 'Email or password miss match';
+            this.errorMessage = 'Email or password mismatch';
           } else {
             this.errorMessage = 'Server communication error';
           }
