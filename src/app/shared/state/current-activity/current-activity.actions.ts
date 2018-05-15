93e9b520b75d93b7bfc3aea24df7a66b5a2e0341
mahsa <molaee.mahsa@gmail.com>
2018-05-15 16:27:39 +0430

    Bug Fix: automatically focus on activity name input after starting a new activity (TG-234)

--- src/app/shared/state/current-activity/current-activity.actions.ts
+++ src/app/shared/state/current-activity/current-activity.actions.ts
@@ -7,11 +7,19 @@ import { ActionWithPayload }  from '../action-with-payload';
 export class CurrentActivityActions {
   static LOAD_CURRENT_ACTIVITY = 'LOAD_CURRENT_ACTIVITY';
   static CLEAR_CURRENT_ACTIVITY = 'CLEAR_ACTIVITY';
+  static RENAME_CURRENT_ACTIVITY = 'RENAME_CURRENT_ACTIVITY';
 
   loadCurrentActivity(activity: Activity): ActionWithPayload<Activity> {
     return {
       type: CurrentActivityActions.LOAD_CURRENT_ACTIVITY,
       payload: activity
+    };
+  }
+
+  renameCurrentActivity(name: string): ActionWithPayload<string> {
+    return {
+      type: CurrentActivityActions.RENAME_CURRENT_ACTIVITY,
+      payload: name
     };
   }
 
