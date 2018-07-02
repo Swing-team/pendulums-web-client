53057e1a5746e9182c5e5780f26f5ac18f0f9a66
mahsa <molaee.mahsa@gmail.com>
2018-07-02 18:01:12 +0430

    Move router listening logic to service
    Manage project recent activities when user manually edit activities list

---
+++ src/app/core/services/roueter-change-listener.service.ts
@@ -0,0 +1,39 @@
+import 'rxjs/add/operator/pairwise';
+import 'rxjs/add/operator/filter';
+import { Subscription }             from 'rxjs/Subscription';
+import { Injectable }               from '@angular/core';
+import { Store }                    from '@ngrx/store';
+import { Router, RoutesRecognized } from '@angular/router';
+import { UserService }              from './user.service';
+import { AppState }                 from '../../shared/state/appState';
+import { ProjectsActions }          from '../../shared/state/project/projects.actions';
+
+@Injectable()
+export class RouterChangeListenerService {
+  private subscriptions: Array<Subscription> = [];
+
+  constructor (private store: Store<AppState>,
+               private router: Router,
+               private userService: UserService,
+               private projectsActions: ProjectsActions) {
+
+    this.subscriptions.push(
+      this.router.events
+        .filter(e => e instanceof RoutesRecognized)
+        .pairwise()
+        .subscribe((e: any) => {
+          if (e[1].url === '/dashboard' && (e[0].url.startsWith('/activities') || e[0].url.startsWith('/profile'))) {
+            this.userService.getSummary()
+              .then((user) => {
+                this.store.dispatch(this.projectsActions.loadProjects(user.projects));
+              })
+              .catch(error => {
+                console.log('error:', error)
+              })
+          }
+        })
+    );
+  }
+}
+
+
