fccf5d77048bb3798d9701be69adfef1fc451f25
mahsa <molaee.mahsa@gmail.com>
2018-08-22 01:48:55 +0430

    Code clean up in stop/start service
    TG-393
    code clean up in toolbar and card item based on stop/start activity service

--- src/app/dashboard/projects/list-project/project-item/project-item.component.ts
+++ src/app/dashboard/projects/list-project/project-item/project-item.component.ts
@@ -4,24 +4,20 @@ import {
 } from '@angular/core';
 import { APP_CONFIG }                         from '../../../../app.config';
 import { Project }                            from '../../../../shared/state/project/project.model';
-import { ActivityService }                    from '../../../shared/activity.service';
 import { Store }                              from '@ngrx/store';
 import { AppState }                           from '../../../../shared/state/appState';
 import { Observable }                         from 'rxjs/Observable';
 import { Activity }                           from '../../../../shared/state/current-activity/current-activity.model';
-import { CurrentActivityActions }             from '../../../../shared/state/current-activity/current-activity.actions';
-import { ProjectsActions }                    from '../../../../shared/state/project/projects.actions';
 import { ModalService }                       from '../../../../core/modal/modal.service';
 import { ProjectSettingsModalComponent }      from 'app/dashboard/projects/settings/modal/project-settings-modal.component';
 import { User }                               from '../../../../shared/state/user/user.model';
 import { Router }                             from '@angular/router';
 import { ErrorService }                       from '../../../../core/error/error.service';
-import { UnSyncedActivityActions }            from '../../../../shared/state/unsynced-activities/unsynced-activities.actions';
 import { Status }                             from '../../../../shared/state/status/status.model';
 import { Md5 }                                from 'ts-md5/dist/md5';
 import { Subscription }                       from 'rxjs/Subscription';
-import { userInProject, userRoleInProject }   from '../../../shared/utils';
-import * as moment from 'moment';
+import { StopStartActivityService }           from '../../../../core/services/stop-start-activity.service';
+import { userInProject }                      from '../../../shared/utils';
 
 @Component({
   selector: 'project-item',
@@ -40,24 +36,17 @@ export class ProjectItemComponent implements OnInit, OnDestroy {
   showMoreStart: number;
   showMoreEnd: number;
 
-  // activityPushType field used to manage the procedure we should update project recent activities in state
-  // if it be "push" means that user is team member or project has just 1 team member and we just need to push activity to recent activities
-  // if it be "edit" means that user is admin/owner and project members are more that 1 so we should update recent activities
-  private activityPushType: string;
   private currentActivityCopy: Activity;
   private taskName: string;
   private activity: Activity;
   private subscriptions: Array<Subscription> = [];
 
   constructor (@Inject(APP_CONFIG) public config,
-               private activityService: ActivityService,
                private store: Store<AppState>,
-               private currentActivityActions: CurrentActivityActions,
-               private projectsActions: ProjectsActions,
                private router: Router,
                private modalService: ModalService,
                private errorService: ErrorService,
-               private unSyncedActivityActions: UnSyncedActivityActions) {
+               private stopStartActivityService: StopStartActivityService) {
   }
 
   ngOnInit() {
@@ -80,7 +69,6 @@ export class ProjectItemComponent implements OnInit, OnDestroy {
       }));
     }
 
-    this.initializeActivityPushType();
     this.initializePointers();
   }
 
@@ -88,19 +76,6 @@ export class ProjectItemComponent implements OnInit, OnDestroy {
     this.subscriptions.map((subscribe) => {
       subscribe.unsubscribe()
     });
-  }
-
-  initializeActivityPushType() {
-    const userRoll = userRoleInProject(this.project, this.user.id);
-
-    this.activityPushType = 'edit';
-
-    if (userRoll === 'team member') {
-      this.activityPushType = 'push';
-
-    } else if (this.project.teamMembers.length === 1) {
-      this.activityPushType = 'push';
-    }
   }
 
   toggleStopStart() {
@@ -115,16 +90,6 @@ export class ProjectItemComponent implements OnInit, OnDestroy {
   }
 
   startActivity() {
-    if (this.currentActivityCopy.startedAt) {
-      this.stopActivity().then(() => {
-        this.startActivityAtServer();
-      });
-    } else {
-      this.startActivityAtServer();
-    }
-  }
-
-  startActivityAtServer() {
     if (!this.taskName) {
       this.taskName = 'untitled activity';
     }
@@ -133,52 +98,11 @@ export class ProjectItemComponent implements OnInit, OnDestroy {
     this.activity.startedAt = Date.now().toString();
     // we need two below fields for offline logic
     this.activity.project = this.project.id;
-    this.activity.user = this.user.id;
 
-    // we decided to put all data in db by default and then send it to server
-    this.store.dispatch(this.currentActivityActions.loadCurrentActivity(this.activity));
-    this.store.dispatch(this.projectsActions.updateSelectedProject(this.activity.project));
-
-    // update project recent activities
-    this.manageProjectRecentActivitiesInState(this.activity.project, this.activity);
-
-    if (this.status.netStatus) {
-      delete this.activity.stoppedAt;
-
-      this.activityService.create(this.project.id, this.activity).then((activity) => {
-        this.showError('The activity started');
-        delete activity.createdAt;
-        delete activity.updatedAt;
-
-        // if we get ok response from server so we have id for currentActivity and it has to been set
-        this.store.dispatch(this.currentActivityActions.loadCurrentActivity(activity));
-        this.activityButtonDisabled = false;
-      })
-        .catch(error => {
-          // todo: check errors
-          if (error.status === 404) {
-            this.showError('Project has been deleted before.');
-            this.store.dispatch(this.projectsActions.removeProject(this.project.id));
-
-            // if we have current activity on deleted project we should clear it
-            if (this.project.id === this.currentActivityCopy.project) {
-              this.store.dispatch(this.currentActivityActions.clearCurrentActivity());
-            }
-          } else if (error.status === 400) {
-            // todo: this section will handle with socket message
-            this.showError('You have current activity on server please refresh your browser.');
-            // we have to clear current activity
-            this.store.dispatch(this.currentActivityActions.clearCurrentActivity());
-          } else {
-            this.showError('Server communication error.');
-          }
-          console.log('server error happened', error);
-          this.activityButtonDisabled = false;
-        });
-    } else {
-      this.showError('The activity started');
+    this.stopStartActivityService.startActivity(this.activity, this.project).then(() => {
       this.activityButtonDisabled = false;
-    }
+      this.showError('The activity started');
+    });
 
     // This timeout use to handle focus on input
     setTimeout(() => {
@@ -189,152 +113,18 @@ export class ProjectItemComponent implements OnInit, OnDestroy {
     }, 500)
   }
 
-  manageProjectRecentActivitiesInState(projectId, activity) {
-    if (this.activityPushType === 'push') {
-      this.store.dispatch(this.projectsActions.addActivityToProject(projectId, activity));
-    } else if (this.activityPushType === 'edit') {
-      this.store.dispatch(this.projectsActions.updateProjectActivities(projectId, activity));
-    }
-  }
-
-  stopActivity(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      if (this.currentActivity) {
-        // first we need divide time to separated days
-        const dividedActivitiesArray = [];
-        const stoppedAtDay = moment().startOf('day');
-        const startedAtDay = moment(Number(this.currentActivityCopy.startedAt)).startOf('day');
-        if (stoppedAtDay.isSame(startedAtDay)) {
-          this.currentActivityCopy.stoppedAt = moment().valueOf().toString();
-        } else {
-          const diff = stoppedAtDay.diff(startedAtDay, 'days');
-          let startedAt = this.currentActivityCopy.startedAt;
-          let stoppedAt = moment(Number(this.currentActivityCopy.startedAt)).endOf('day').valueOf();
-          this.currentActivityCopy.stoppedAt = stoppedAt.toString();
-          for (let i = 0; i < diff; i++) {
-            startedAt = (stoppedAt + 1).toString();
-            if ( i < diff - 1) {
-              stoppedAt = moment(stoppedAt + 1).endOf('day').valueOf();
-            } else if (i === diff - 1) {
-              stoppedAt = moment().valueOf();
-            }
-            const tempResult = {
-              name: this.currentActivityCopy.name,
-              user: this.currentActivityCopy.user,
-              project: this.currentActivityCopy.project,
-              startedAt: startedAt,
-              stoppedAt: stoppedAt.toString(),
-            };
-            dividedActivitiesArray.push(tempResult);
-          }
-        }
-
-        // we must save divided activities and original activity in db
-        this.pushDividedActivitiesToDb(dividedActivitiesArray);
-
-        if (this.status.netStatus) {
-          // now we will try to store all data at server
-          if (this.currentActivityCopy.id) {
-            this.activityService.editCurrentActivity(this.project.id, this.currentActivityCopy).then((activity) => {
-              this.updateStateInSuccess(dividedActivitiesArray);
-              resolve();
-            })
-              .catch(error => {
-                this.updateStateInCatch(error);
-                resolve();
-              });
-          } else {
-            this.activityService.createManually(this.project.id, this.currentActivityCopy).then((activity) => {
-              this.updateStateInSuccess(dividedActivitiesArray);
-              resolve();
-            })
-              .catch((error) => {
-                this.updateStateInCatch(error);
-                resolve();
-              });
-          }
-        } else {
-          this.store.dispatch(this.currentActivityActions.clearCurrentActivity());
-          this.activityButtonDisabled = false;
-          this.showError('The activity stopped');
-          resolve();
-        }
-      }
-    });
-  }
-
-  pushDividedActivitiesToDb (dividedActivitiesResult) {
-    // store an original activity
-    this.store.dispatch(this.unSyncedActivityActions.addUnSyncedActivity(this.currentActivityCopy));
-    this.store.dispatch(this.projectsActions.updateProjectActivities(this.currentActivityCopy.project, this.currentActivityCopy));
-    // store all divided activities
-    dividedActivitiesResult.map((item) => {
-      this.store.dispatch(this.unSyncedActivityActions.addUnSyncedActivity(item));
-      this.store.dispatch(this.projectsActions.updateProjectActivities(item.project, item));
-    });
-  }
-
-  pushDividedActivitiesToServer (dividedActivitiesResult) {
-    const responseResult = [];
-    dividedActivitiesResult.map((item) => {
-      responseResult.push(this.activityService.createManually(this.project.id, item).then((activity) => {
-        this.store.dispatch(this.unSyncedActivityActions.removeUnSyncedActivityByFields(item))
-      })
-        .catch(error => {
-          console.log('server error happened', error);
-        }));
-    });
-    Promise.all(responseResult).then(() => {
+  stopActivity() {
+    this.stopStartActivityService.stopActivity().then(() => {
       this.activityButtonDisabled = false;
-    })
-  }
-
-  updateStateInCatch (error) {
-    console.log('server error happened', error);
-    this.showError('Server communication error.');
-    this.showError('The activity stopped');
-    this.activityButtonDisabled = false;
-    this.store.dispatch(this.currentActivityActions.clearCurrentActivity());
-  }
-
-  updateStateInSuccess (dividedActivitiesArray) {
-    this.store.dispatch(this.unSyncedActivityActions.removeUnSyncedActivityByFields(this.currentActivityCopy));
-    // we send divided activities to server after original activity because
-    // The stop time of activity cannot be older than start time in current activity!
-    this.pushDividedActivitiesToServer(dividedActivitiesArray);
-    this.showError('The activity stopped');
-    this.store.dispatch(this.currentActivityActions.clearCurrentActivity());
+      this.showError('The activity stopped');
+    });
   }
 
   nameActivity($event) {
+    this.stopStartActivityService.nameActivity(this.taskName);
     // just for blur out the input
     const target = $event.target;
     target.blur();
-
-    if (this.currentActivity) {
-      this.currentActivityCopy.name = this.taskName;
-      this.store.dispatch(this.currentActivityActions.renameCurrentActivity(this.currentActivityCopy.name));
-      this.store.dispatch(this.projectsActions.updateProjectActivities(this.currentActivityCopy.project, this.currentActivityCopy));
-      if (this.status.netStatus) {
-        delete this.currentActivityCopy.stoppedAt;
-
-        if (this.currentActivityCopy.id) {
-          this.activityService.editCurrentActivity(this.project.id, this.currentActivityCopy).then((activity) => {
-          })
-            .catch(error => {
-              console.log('server error happened', error);
-            });
-        } else {
-          this.activityService.create(this.project.id, this.currentActivityCopy).then((activity) => {
-            // we reload currentActivity because it will get id and we will need it
-            this.store.dispatch(this.currentActivityActions.loadCurrentActivity(activity));
-          })
-            .catch(error => {
-              console.log('server error happened', error);
-            });
-        }
-      }
-    }
   }
 
   calculateActivityDuration (activity) {
