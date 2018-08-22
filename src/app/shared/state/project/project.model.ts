a25bfa1f58c80d35b7d897c861d0a353b6f08a6d
mahsa <molaee.mahsa@gmail.com>
2018-08-22 23:38:54 +0430

    Added 2 new colorPalette

--- src/app/shared/state/project/project.model.ts
+++ src/app/shared/state/project/project.model.ts
@@ -10,7 +10,7 @@ export class Project {
   teamMembers: Array<TeamMember>;
   admins: Array<TeamMember>;
   recentActivityName: string = null;
-  // [white == 0, salmon == 1, blue == 2, black == 3, yellow == 4, pink == 5]
+  // [white == 0, salmon == 1, blue == 2, black == 3, yellow == 4, pink == 5, red == 6, green == 7]
   colorPalette = 0;
 
   constructor() {}
