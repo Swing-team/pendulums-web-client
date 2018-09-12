a877e1b2f49eae1ed77b0b1052b7e8bfd524634c
Ashkan Tofangdar <ashkan4472@gmail.com>
2018-09-12 20:37:13 +0430

    realtime correction

--- src/app/core/services/sync.service.ts
+++ src/app/core/services/sync.service.ts
@@ -38,7 +38,6 @@ export class SyncService {
               private projectsActions: ProjectsActions,
               private currentActivityActions: CurrentActivityActions,
               private unSyncedActivityActions: UnSyncedActivityActions,
-              private syncService: SyncService,
               private appService: AppService) {
     this.status = store.select('status');
     this.currentActivity = store.select('currentActivity');
@@ -66,7 +65,7 @@ export class SyncService {
 
   syncData(data): Promise<any> {
     return this.http
-      .put(this.config.apiEndpoint + '/sync/activities' + '/activities?socketId=' + this.syncService.getSocketId()
+      .put(this.config.apiEndpoint + '/sync/activities' + '/activities?socketId=' + this.getSocketId()
       , JSON.stringify(data), {...this.config.httpOptions, responseType: 'text'})
       .toPromise()
       .then(() => {
