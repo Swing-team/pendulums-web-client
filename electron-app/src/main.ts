0a6995cf6f2616d30bb8da847a85c77de10cf951
Armin Ghoreishi <armin.ghoreishi@gmail.com>
2018-09-16 19:03:10 +0430

    Hide the devconsole at startup

--- electron-app/src/main.ts
+++ electron-app/src/main.ts
@@ -144,7 +144,7 @@ const createWindow = () => {
 
 
     // win.loadURL('http://localhost:4200');
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     // Emitted when the window is closed.
     win.on('closed', () => {
