6769523a770bf0b0a3fe68007fa593794e7d2131
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2019-08-04 16:33:45 +0430

    Added leaving project UI
    TG-685
    TG-691

--- src/app/core/auth.interceptor.ts
+++ src/app/core/auth.interceptor.ts
@@ -24,7 +24,7 @@ export class AuthInterceptor implements HttpInterceptor {
     }, (error: any) => {
       if (error instanceof HttpErrorResponse) {
         if ((error.status === 403) && (window.location.href.match(/\?/g) || []).length < 2) {
-            console.log('The authentication session expires or the user is not authorized. Force refresh of the current page.');
+          console.log('The authentication session expires or the user is not authorized. Force refresh of the current page.');
           this.store.dispatch(this.statusActions.updateIsLogin(false));
         } else if (error.status === 401) {
           this.router.navigate(['dashboard']);
