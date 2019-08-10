a3abc72836b1c170a15bc6ae6a2de3474674f956
Ashkan Tofangdar <ashkan4472@gmail.com>
2019-08-10 14:28:48 +0430

    added error message
    TG-696

--- src/app/profile-setting/delete-account/delete-account.component.ts
+++ src/app/profile-setting/delete-account/delete-account.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input }                              from '@angular/core';
+import { Component, Input }                       from '@angular/core';
 import { ModalService }                           from '../../core/modal/modal.service';
 import { UserActions }                            from '../../shared/state/user/user.actions';
 import { UserService }                            from '../../core/services/user.service';
-import { User } from 'app/shared/state/user/user.model';
-import { Store } from '@ngrx/store';
-import { AppState } from 'app/shared/state/appState';
+import { User }                                   from 'app/shared/state/user/user.model';
+import { Store }                                  from '@ngrx/store';
+import { AppState }                               from 'app/shared/state/appState';
+import { ErrorService }                           from 'app/core/error/error.service';
 
 @Component({
   selector: 'delete-account',
@@ -20,7 +21,8 @@ export class DeleteAccountComponent {
   constructor(private modalService: ModalService,
               private userActions: UserActions,
               private userService: UserService,
-              private store: Store<AppState>) { }
+              private store: Store<AppState>,
+              private errorService: ErrorService) { }
 
   cancel() {
     if (!this.disableButtons) {
@@ -40,9 +42,20 @@ export class DeleteAccountComponent {
         .catch(error => {
           console.log('error is: ', error);
           this.disableButtons = false;
+          if (error.status === 401) {
+            this.showError(`Incorrect Password!`);
+          } else {
+            this.showError(`Something went wrong! please try again`);
+          }
           this.modalService.close();
         });
     }
+  }
+
+  showError(error) {
+    this.errorService.show({
+      input: error
+    });
   }
 
 }
