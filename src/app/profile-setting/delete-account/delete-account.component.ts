7164fd57ebffd43b44f0ee2d4e31919fc14cd0dc
Ashkan Tofangdar <ashkan4472@gmail.com>
2019-08-18 12:00:38 +0430

    remved loacl database in delete account
    TG-697

--- src/app/profile-setting/delete-account/delete-account.component.ts
+++ src/app/profile-setting/delete-account/delete-account.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input }                       from '@angular/core';
+import { Component }                              from '@angular/core';
+import { Router }                                 from '@angular/router';
 import { ModalService }                           from '../../core/modal/modal.service';
 import { UserActions }                            from '../../shared/state/user/user.actions';
 import { UserService }                            from '../../core/services/user.service';
-import { User }                                   from 'app/shared/state/user/user.model';
 import { Store }                                  from '@ngrx/store';
 import { AppState }                               from 'app/shared/state/appState';
 import { ErrorService }                           from 'app/core/error/error.service';
+import { DatabaseService }                        from 'app/core/services/database/database.service';
 
 @Component({
   selector: 'delete-account',
@@ -15,12 +16,13 @@ import { ErrorService }                           from 'app/core/error/error.ser
 export class DeleteAccountComponent {
   disableButtons = false;
   userPassword: string;
-  subscriptions: any;
 
   constructor(private modalService: ModalService,
               private userActions: UserActions,
               private userService: UserService,
               private store: Store<AppState>,
+              private databaseService: DatabaseService,
+              private router: Router,
               private errorService: ErrorService) { }
 
   cancel() {
@@ -32,19 +34,24 @@ export class DeleteAccountComponent {
   delete() {
     if (!this.disableButtons) {
       this.disableButtons = true;
-
-      this.userService.deleteAccount(this.userPassword).then(() => {
-        this.store.dispatch(this.userActions.clearUser());
+      this.userService.deleteAccount(this.userPassword).then((userId) => {
         this.disableButtons = false;
         this.modalService.close();
+        this.store.dispatch(this.userActions.clearUser());
+        this.databaseService.remove('appInfo', userId);
+        this.databaseService.remove('userData', userId);
+        this.databaseService.removeAll('activeUser');
+        this.router.navigate(['signIn']);
       })
         .catch(error => {
           console.log('error is: ', error);
           this.disableButtons = false;
-          if (error.status === 401) {
+          if (error.status === 400) {
             this.showError(`Incorrect Password!`);
+          } else if (error.status === 403) {
+            this.showError(`you are not allow to perform this action`);
           } else {
-            this.showError(`Something went wrong! please try again`);
+            this.showError('Something went wrong! please try again');
           }
           this.modalService.close();
         });
