8c7d9f8e610dd05753c02540524b4f0888086219
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-08-03 12:26:06 +0430

    fixed some bugs in top bar
    TG-774

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit, OnDestroy, DoCheck, KeyValueDiffers } from '@angular/core';
 import { SideMenuService } from 'app/core/services/side-menu.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { User } from '../state/user/user.model';
 import { Status } from '../state/status/status.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../state/appState';
 
 @Component({
   selector: 'top-bar',
@@ -10,9 +12,11 @@ import { Status } from '../state/status/status.model';
   styleUrls: ['./top-bar.component.sass'],
 })
 export class TopBarComponent implements OnInit, OnDestroy, DoCheck {
-  @Input() user: User;
-  @Input() status: Status;
   @Input() title: string;
+  user$: Observable<User>;
+  user: User;
+  status$: Observable<Status>
+  status: Status;
   hasNotification: boolean;
   isSideMenuActive: boolean;
   subscriptions: Subscription[] = [];
@@ -20,13 +24,19 @@ export class TopBarComponent implements OnInit, OnDestroy, DoCheck {
   
   constructor(
     private readonly sideMenuService: SideMenuService,
+    private store: Store<AppState>,
     private differs: KeyValueDiffers,
   ) {
+    this.status$ = store.select('status')
+    this.user$ = store.select('user');
     this.differ = this.differs.find({}).create();
   }
 
 
   ngOnInit() {
+    this.subscriptions.push(this.status$.subscribe((status) => this.status = status));
+    this.subscriptions.push(this.user$.subscribe((user) => this.user = user));
+
     this.subscriptions.push(
       this.sideMenuService.getIsSideMenuActiveAsObservable().subscribe((isSideMenuActive) => {
         this.isSideMenuActive = isSideMenuActive;
