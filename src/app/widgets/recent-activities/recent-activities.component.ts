1086b3ce84db33bb4e3d4adc1d392b371d3aa2f7
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-06 15:11:16 +0430

    added buttons fucntionality in recent activities widget

--- src/app/widgets/recent-activities/recent-activities.component.ts
+++ src/app/widgets/recent-activities/recent-activities.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnDestroy } from '@angular/core';
 import { RecentActivityWithProject } from './model/recent-activities-with-project.model';
 import { User } from 'app/shared/state/user/user.model';
 import { ErrorService } from 'app/core/error/error.service';
+import { ModalService } from 'app/core/modal/modal.service';
+import { AddManuallyActivityComponent } from 'app/activity/activity-add-edit-manually/activity-add-edit-manually.component';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'app/shared/state/appState';
+import { Activity } from 'app/shared/state/current-activity/current-activity.model';
 
 @Component({
   selector: 'recent-activities',
   templateUrl: './recent-activities.component.html',
   styleUrls: ['./recent-activities.component.sass'],
 })
-export class RecentActivitiesComponent implements OnInit {
+export class RecentActivitiesComponent implements OnInit, OnDestroy {
   @Input() recentActivitiesWithProject: RecentActivityWithProject[];
   @Input() currentUser: User;
   @Input() matchHeightWithParent: boolean;
   @Input() matchWidthWithParent: boolean;
   @Input() emptyPlaceHolder: string;
+  @Input() showFooterPagination: boolean
+  @Input() projectId: string;
 
+  subscriptions: Subscription[] = [];
+  currentActivity: Observable<Activity>;
   activitiesPagination: Array<Array<RecentActivityWithProject>> = [];
   activitiesPaginationIndex = 0;
 
@@ -22,7 +32,11 @@ export class RecentActivitiesComponent implements OnInit {
 
   constructor(
     private errorService: ErrorService,
-  ) { }
+    private modalService: ModalService,
+    private store: Store<AppState>,
+  ) {
+    this.currentActivity = store.select('currentActivity');
+  }
 
   ngOnInit() {
     if (this.recentActivitiesWithProject) {
@@ -34,6 +48,10 @@ export class RecentActivitiesComponent implements OnInit {
     if (changes && changes.recentActivitiesWithProject) {
       this.paginateRecentActivities();
     }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
   paginateRecentActivities() {
@@ -63,6 +81,29 @@ export class RecentActivitiesComponent implements OnInit {
     } else {
       this.showError('There are no more current activities');
     }
+  }
+
+  openAddManuallyModal() {
+    let tempCurrentActivity: any;
+    if (this.currentActivity) {
+      this.subscriptions.push(this.currentActivity.subscribe(currentActivity => {
+        tempCurrentActivity = currentActivity;
+      }));
+    }
+    this.modalService.show({
+      component:  AddManuallyActivityComponent,
+      inputs: {
+        projectId: this.projectId,
+        currentActivity: tempCurrentActivity ? tempCurrentActivity : null,
+      },
+      // there is output if needed
+      // outputs: {
+      //   responseActivity: (params) => {
+          
+      //   }
+      // },
+      customStyles: {'width': '400px'}
+    });
   }
 
   showError(error) {
